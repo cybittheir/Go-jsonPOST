99b82f009cf4ecc9055acd899f37dc6e5cce84bb
Александр Ловин <kibitzer.vl@hotmail.com>
2022-10-06 21:34:21 +1000

    first version

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+)
+
+var (
+	version string
+	build   string
+)
+
+func main() {
+
+	// Open our jsonFile
+
+	jsonFile, err := os.Open("conf.json")
+
+	// if we os.Open returns an error then handle it
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened conf.json")
+
+	// defer the closing of our jsonFile so that we can parse it later on
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+
+	var confResult map[string]map[string]string
+
+	json.Unmarshal([]byte(byteValue), &result)
+	json.Unmarshal([]byte(byteValue), &confResult)
+
+	postprotocol := fmt.Sprintf("%s", confResult["Responder"]["protocol"])
+
+	testport := "23"
+
+	if postprotocol == "https" {
+
+		testport = "443"
+
+	} else if postprotocol == "http" {
+
+		testport = "80"
+
+	} else if postprotocol == "ftp" {
+
+		testport = "21"
+	}
+
+	ResponerIPAddr := fmt.Sprintf("%s:%s", confResult["Responder"]["oldip4"], testport)
+	httpposturl := fmt.Sprintf("%s://%s/%s", confResult["Responder"]["protocol"], confResult["Responder"]["oldip4"], confResult["Responder"]["confURL"])
+
+	jsonPost, err := json.Marshal(result["newconfig"])
+
+	fmt.Println("Trying connect to " + ResponerIPAddr)
+	tcpAddr, _ := net.ResolveTCPAddr("tcp4", ResponerIPAddr)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+
+	if err == nil {
+
+		conn.Close()
+
+		fmt.Println("Connected successful to " + ResponerIPAddr)
+
+		fmt.Println("Map of new config: ", result["newconfig"])
+
+		fmt.Println("HTTP JSON POST URL:", httpposturl)
+
+		fmt.Println("Sending POST request...")
+
+		request, error := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonPost))
+		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+		client := &http.Client{}
+		response, error := client.Do(request)
+
+		if error != nil {
+
+			panic(error)
+
+		}
+
+		defer response.Body.Close()
+
+		fmt.Println("response Status:", response.Status)
+		fmt.Println("response Headers:", response.Header)
+
+		body, _ := ioutil.ReadAll(response.Body)
+
+		fmt.Println("response Body:", string(body))
+
+	} else {
+
+		fmt.Println("No connection to " + ResponerIPAddr)
+
+	}
+
+}
